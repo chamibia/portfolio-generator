0603e8be5ca3186cc694a14bd68634b6120d0214
chamibia <b.chami39@gmail.com>
2021-12-06 13:57:16 -0500

    new file and function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const profileDateArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDateArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
-printProfileData(profileDateArgs);
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile("./index.html", pageHTML, (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
