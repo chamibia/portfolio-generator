62bd4e727614a0cd89e79953bb7713d02afc2873
chamibia <b.chami39@gmail.com>
2021-12-09 18:48:07 -0500

    finished template

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require("inquirer");
-//const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -59,7 +52,7 @@ const promptUser = () => {
   ]);
 };
 
-promptUser().then((answers) => console.log(answers));
+// promptUser().then((answers) => console.log(answers));
 
 const promptProject = (portfolioData) => {
   console.log(`
@@ -153,5 +146,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("index.html", pageHTML, (err) => {
+      if (err) throw err;
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
